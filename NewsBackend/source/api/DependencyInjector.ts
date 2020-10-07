328062b8a1873497efa0446b0c95d528c4ad32e2
carlCarlson6 <bullet.carlos@gmail.com>
2020-10-07 23:17:58 +0200

    updated readme

--- NewsBackend/source/api/DependencyInjector.ts
+++ NewsBackend/source/api/DependencyInjector.ts
@@ -1,6 +1,5 @@
 import { InjectionToken, Provider, ReflectiveInjector } from "injection-js";
 import { NewsController } from "./controllers/NewsController";
-import { NewsRoutes } from "./routes/NewsRoutes";
 
 
 export const NEWS_CONTROLLER = new InjectionToken<NewsController>('NEWS_CONTROLLER');
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## NEWS BACKEND
 
 ![components_diagram](img/components_diagram.jpg)
+![API_diagram](img/API_diagram.jpg)
